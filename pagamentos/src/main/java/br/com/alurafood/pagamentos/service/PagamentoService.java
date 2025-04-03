f61e049a4456808d7fea54afd821a039a4ecde32
Rayner Santana <raynersan@hotmail.com>
2025-04-03 18:49:35 -0300

    Adicionando CircuitBreaker à confirmação de pagamento

--- pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
+++ pagamentos/src/main/java/br/com/alurafood/pagamentos/service/PagamentoService.java
@@ -70,4 +70,15 @@ public class PagamentoService {
         pagamentoRepository.save(pagamento.get());
         pedido.atualizaPagamento(pagamento.get().getPedidoId());
     }
+
+    public void alteraStatus(Long id) {
+        Optional<Pagamento> pagamento = pagamentoRepository.findById(id);
+
+        if (!pagamento.isPresent()) {
+            throw new EntityNotFoundException();
+        }
+
+        pagamento.get().setStatus(Status.CONFIRMADO_SEM_INTEGRACAO);
+        pagamentoRepository.save(pagamento.get());
+    }
 }
